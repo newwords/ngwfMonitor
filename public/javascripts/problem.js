a4f4097e1917b847a71a032a21dcdd9223197f50
zhangxin <149245661@qq.com>
2017-09-20 17:05:59 +0800

    页面

--- public/javascripts/problem.js
+++ public/javascripts/problem.js
@@ -1,4 +1,4 @@
-require(['backbone', 'handlebars', 'text!tpl/plan.hbs',
+require(['backbone', 'handlebars', 'text!tpl/problem.hbs',
     'layui'], function (Backbone, Handlebars, tpl) {
     var _content = this;
     var PlanListView = Backbone.View.extend({
