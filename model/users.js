a2a9be8cde23e482aa06132b1a99284f3c2bb6e4
whyang115 <whyang115@gmail.com>
2017-09-20 20:36:55 +0800

    user complete

--- model/users.js
+++ model/users.js
@@ -1,35 +1,163 @@
 "use strict";
 module.exports = [
-  { name: 'zhangxin', password: '123456' },
-  { name: 'beijing', password: '123456' },
-  { name: 'tianjin', password: '123456' },
-  { name: 'neimenggu', password: '123456' },
-  { name: 'hebei', password: '123456' },
-  { name: 'heilongjiang', password: '123456' },
-  { name: 'liaoning', password: '123456' },
-  { name: 'jilin', password: '123456' },
-  { name: 'shandong', password: '123456' },
-  { name: 'henan', password: '123456' },
-  { name: 'shanxi', password: '123456' },
-  { name: 'shanxi3', password: '123456' },
-  { name: 'gansu', password: '123456' },
-  { name: 'ningxia', password: '123456' },
-  { name: 'xinjiang', password: '123456' },
-  { name: 'xizang', password: '123456' },
-  { name: 'yunnan', password: '123456' },
-  { name: 'sichuan', password: '123456' },
-  { name: 'chongqing', password: '123456' },
-  { name: 'hubei', password: '123456' },
-  { name: 'hunan', password: '123456' },
-  { name: 'jiangsu', password: '123456' },
-  { name: 'jiangxi', password: '123456' },
-  { name: 'zhejiang', password: '123456' },
-  { name: 'fujian', password: '123456' },
-  { name: 'guangdong', password: '123456' },
-  { name: 'guangxi', password: '123456' },
-  { name: 'guizhou', password: '123456' },
-  { name: 'shanghai', password: '123456' },
-  { name: 'hainan', password: '123456' },
-  { name: 'anhui', password: '123456' },
-  { name: 'qinghai', password: '123456' }
+  {
+    name: 'zhangxin',
+    password: '123456',
+    province: "00030001"
+  },
+  {
+    name: 'beijing',
+    password: '123456',
+    province: "00030001"
+  },
+  {
+    name: 'tianjin',
+    password: '123456',
+    province: "00030002"
+  },
+  {
+    name: 'neimenggu',
+    password: '123456',
+    province: "00030003"
+  },
+  {
+    name: 'hebei',
+    password: '123456',
+    province: "00030004"
+  },
+  {
+    name: 'heilongjiang',
+    password: '123456',
+    province: "00030005"
+  },
+  {
+    name: 'liaoning',
+    password: '123456',
+    province: "00030006"
+  },
+  {
+    name: 'jilin',
+    password: '123456',
+    province: "00030007"
+  },
+  {
+    name: 'shandong',
+    password: '123456',
+    province: "00030008"
+  },
+  {
+    name: 'henan',
+    password: '123456',
+    province: "00030009"
+  },
+  {
+    name: 'shanxi',
+    password: '123456',
+    province: "00030010"
+  },
+  {
+    name: 'shanxi3',
+    password: '123456',
+    province: "00030011"
+  },
+  {
+    name: 'gansu',
+    password: '123456',
+    province: "00030012"
+  },
+  {
+    name: 'ningxia',
+    password: '123456',
+    province: "00030013"
+  },
+  {
+    name: 'xinjiang',
+    password: '123456',
+    province: "00030014"
+  },
+  {
+    name: 'xizang',
+    password: '123456',
+    province: "00030015"
+  },
+  {
+    name: 'yunnan',
+    password: '123456',
+    province: "00030016"
+  },
+  {
+    name: 'sichuan',
+    password: '123456',
+    province: "00030017"
+  },
+  {
+    name: 'chongqing',
+    password: '123456',
+    province: "00030018"
+  },
+  {
+    name: 'hubei',
+    password: '123456',
+    province: "00030019"
+  },
+  {
+    name: 'hunan',
+    password: '123456',
+    province: "00030020"
+  },
+  {
+    name: 'jiangsu',
+    password: '123456',
+    province: "00030021"
+  },
+  {
+    name: 'jiangxi',
+    password: '123456',
+    province: "00030022"
+  },
+  {
+    name: 'zhejiang',
+    password: '123456',
+    province: "00030023"
+  },
+  {
+    name: 'fujian',
+    password: '123456',
+    province: "00030024"
+  },
+  {
+    name: 'guangdong',
+    password: '123456',
+    province: "00030025"
+  },
+  {
+    name: 'guangxi',
+    password: '123456',
+    province: "00030026"
+  },
+  {
+    name: 'guizhou',
+    password: '123456',
+    province: "00030027"
+  },
+  {
+    name: 'shanghai',
+    password: '123456',
+    province: "00030028"
+  },
+  {
+    name: 'hainan',
+    password: '123456',
+    province: "00030029"
+  },
+  {
+    name: 'anhui',
+    password: '123456',
+    province: "00030030"
+  },
+  {
+    name: 'qinghai',
+    password: '123456',
+    province: "00030031"
+  }
 ];
