01262a8f0ca564038bbfe872ef5aa18a8b32faac
zhangxin <149245661@qq.com>
2017-09-20 16:18:08 +0800

    丢到主机上试试

---
+++ public/javascripts/plan.js
@@ -0,0 +1,7 @@
+require(['backbone', 'handlebars', 'text!tpl/plan.hbs',
+    'layui'], function (Backbone, Handlebars, tpl) {
+    var _content = this;
+    var PlanListView = Backbone.View.extend({});
+
+});
+
